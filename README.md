f55578f7604d50ac9f62e3bd08c90a2edf45090d
chris <cmorrison82z@yahoo.com>
2023-08-26 16:35:43 +0100

    fixed indentation. removed moonwave

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+## Notes
+
+- `before` and `after` jobs have been removed for now.
 
 ## Example job instructions handlers
+
 ```lua
 -- Default behavior :
 local function default_job_getter(f)
@@ -9,17 +13,16 @@ end
 do
 
     -- Distribute job instructions to actor
-local function rbx_disributer(instructions_list, ...)
-    local next_actor = next(actors)
+    local function rbx_disributer(instructions_list, ...)
+        local next_actor = next(actors)
 
-    next_actor:SendMessage(instructions_list, ...)
-end
+        next_actor:SendMessage(instructions_list, ...)
+    end
 
     -- Example implementation in an Actor script to follow job retrieval instruction.
--- TODO: memoize already required modules ?
-local function roblox_job_getter(instructions)
-    return require(instructions.Module)[instructions.Name]
-end
-
+    -- TODO: memoize already required modules ?
+    local function roblox_job_getter(instructions)
+        return require(instructions.Module)[instructions.Name]
+    end
 end
 ```
--- src/init.lua
+++ src/init.lua
@@ -2,91 +2,92 @@ local coroutine_wrap = coroutine.wrap
 
 local Sandwich = {}
 
-Sandwich.Distributer = function (instructions, ...)
-    coroutine_wrap(instructions)(...)
+Sandwich.Distributer = function(instructions, ...)
+	coroutine_wrap(instructions)(...)
 end
 
 function Sandwich.schedule()
 	local schedule = {
-        job_dependencies = {},
+		job_dependencies = {},
 		jobs = {},
 	}
 
 	function schedule.job(job_instructions, ...)
-		schedule.job_dependencies[job_instructions] = {...}
-        table.insert(schedule.jobs, job_instructions)
+		schedule.job_dependencies[job_instructions] = { ... }
+		table.insert(schedule.jobs, job_instructions)
 
 		return job_instructions
 	end
 
-    function schedule.start(...)
-        local job_dependencies = schedule.job_dependencies
+	function schedule.start(...)
+		local job_dependencies = schedule.job_dependencies
 
-        local _incomplete_job_threads_list = {}
-        local _incomplete_job_threads_index = {}
+		local _incomplete_job_threads_list = {}
+		local _incomplete_job_threads_index = {}
 
-        local _completed_jobs = {}
+		local _completed_jobs = {}
 
-        -- local before = schedule.before
-        -- local after = schedule.after
+		-- local before = schedule.before
+		-- local after = schedule.after
 
-        local function job_run(job, ...)
-            local _this_job_dependencies = job_dependencies[job]
-            local _completed_dependencies = 0
+		local function job_run(job, ...)
+			local _this_job_dependencies = job_dependencies[job]
+			local _completed_dependencies = 0
 
-            local _can_proceed = _completed_dependencies == #_this_job_dependencies
+			local _can_proceed = _completed_dependencies == #_this_job_dependencies
 
-            while not _can_proceed do
-                _completed_dependencies = 0
+			while not _can_proceed do
+				_completed_dependencies = 0
 
-                for _, _o_job in ipairs(_this_job_dependencies) do
-                    if not _completed_jobs[_o_job] then break end
+				for _, _o_job in ipairs(_this_job_dependencies) do
+					if not _completed_jobs[_o_job] then
+						break
+					end
 
-                    _completed_dependencies = _completed_dependencies + 1
-                end
+					_completed_dependencies = _completed_dependencies + 1
+				end
 
+				_can_proceed = _completed_dependencies == #_this_job_dependencies
 
-                _can_proceed = _completed_dependencies == #_this_job_dependencies
+				coroutine.yield()
+			end
 
-                coroutine.yield()
-            end
+			Sandwich.Distributer(job, ...)
 
-            Sandwich.Distributer(job, ...)
+			local _last_job = _incomplete_job_threads_list[#_incomplete_job_threads_list]
+			_incomplete_job_threads_list[_incomplete_job_threads_index[job]] = _last_job
+			_incomplete_job_threads_list[#_incomplete_job_threads_list] = nil
 
-            local _last_job = _incomplete_job_threads_list[#_incomplete_job_threads_list]
-            _incomplete_job_threads_list[_incomplete_job_threads_index[job]] = _last_job
-            _incomplete_job_threads_list[#_incomplete_job_threads_list] = nil
+			_incomplete_job_threads_index[_last_job.Job] = _incomplete_job_threads_index[job]
+			_incomplete_job_threads_index[job] = nil
 
-            _incomplete_job_threads_index[_last_job.Job] = _incomplete_job_threads_index[job]
-            _incomplete_job_threads_index[job] = nil
-
-            _completed_jobs[job] = true
-        end
-
-		for _, job in ipairs(schedule.jobs) do
-            local _thread = coroutine_wrap(job_run)
-
-            table.insert(_incomplete_job_threads_list, {
-                Thread = _thread,
-                Job = job
-            })
-
-            _incomplete_job_threads_index[job] = #_incomplete_job_threads_list
-
-            _thread(job, ...)
+			_completed_jobs[job] = true
 		end
 
-        local i;
+		for _, job in ipairs(schedule.jobs) do
+			local _thread = coroutine_wrap(job_run)
 
-        while #_incomplete_job_threads_list > 0 do
-            i = 1
+			table.insert(_incomplete_job_threads_list, {
+				Thread = _thread,
+				Job = job,
+			})
 
-            while _incomplete_job_threads_list[i] do
-                _incomplete_job_threads_list[i].Thread()
+			_incomplete_job_threads_index[job] = #_incomplete_job_threads_list
 
-                i = i + 1
-            end
-        end
+			_thread(job, ...)
+		end
+
+		local i
+
+		while #_incomplete_job_threads_list > 0 do
+			i = 1
+
+			while _incomplete_job_threads_list[i] do
+				_incomplete_job_threads_list[i].Thread()
+
+				i = i + 1
+			end
+		end
 	end
 
 	return schedule
@@ -102,5 +103,3 @@ function Sandwich.interval(seconds, callback, ...)
 end
 
 return Sandwich
-
-
